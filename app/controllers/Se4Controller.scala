82e205b579de29a885086d07a9020e39b3366d9e
Dale Wijnand <dale.wijnand@gmail.com>
2015-06-08 10:21:08 +0100

    Flesh out ServiceStatus.

--- app/controllers/Se4Controller.scala
+++ app/controllers/Se4Controller.scala
@@ -1,9 +1,42 @@
 package controllers
 
+import org.joda.time.{ Duration, DateTime }
 import play.api.mvc.{ Action, Controller }
+
+import java.net.URI
 
 class Se4Controller extends Controller {
   def getServiceStatus      = Action(Ok(views.html.index("Your new application is ready.")))
   def getServiceHealthcheck = Action(Ok(views.html.index("Your new application is ready.")))
   def getServiceGtg         = Action(Ok(views.html.index("Your new application is ready.")))
 }
+
+// TODO: Ask Glen about below being of type float instead of string
+final case class ServiceStatus(
+  group_id         : String,
+  artifact_id      : String,
+  version          : String,
+  git_sha1         : String,
+
+  built_by         : String,
+  build_number     : String,
+  build_machine    : String,
+  built_when       : String,
+  compiler_version : String,
+
+  machine_name     : String,
+  current_time     : DateTime,
+  up_since         : DateTime,
+  up_duration      : Duration,
+
+  os_arch          : String,
+  os_numprocessors : Option[String],
+  os_name          : String,
+  os_version       : String,
+  os_avgload       : Option[String],
+
+  vm_name          : String,
+  vm_vendor        : String,
+  vm_version       : String,
+  runbook_url      : URI
+)
